cc37fdc06ba76021edda1816f66dcd4470a92f45
vrashad <154592842+vrashad@users.noreply.github.com>
2024-01-06 15:06:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import imutils
 import easyocr
 
 # 1. İlk olaraq şəkil faylını Image, Grayscale, Blur rejimində oxuyuruq
-img = cv2.imread('./images/car1.jpg')
+img = cv2.imread('car1.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
 
